d1b87cfbbecccc23f26d9a0cf0efd98cc2fb48e3
mthrib <matheuseyorrana@gmail.com>
2025-03-13 21:21:35 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,4 @@
 # Ecreva um algoritmo para ler um valor numérico ( do teclado) e escrever (na tela) o seu antecessor
 print("Qual o Número? ")
 numero = int(input())
-print("Seu antecessor é: ")
-print(numero - 1)
+print("Seu antecessor é: ", numero - 1)
