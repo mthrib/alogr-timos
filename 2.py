b5b779f3cc049257236b98a18afdeb2b5c5de4c2
mthrib <matheuseyorrana@gmail.com>
2025-03-13 21:42:30 -0300

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,7 @@
+# escreva um algoritmo para ler as dimensôes de um retângulo (base e altura), calcular e escrever a área do retângulo
+print("Qual a base?")
+base = float(input())
+print("Qual a altura?")
+altura = float(input())
+area = base * altura
+print("Sua área do retângulo é:", area)
