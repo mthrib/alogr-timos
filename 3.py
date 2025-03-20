8f0760cff5687e784d8ad7b58838c48f6ff81956
mthrib <matheuseyorrana@gmail.com>
2025-03-20 19:06:41 -0300

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,10 @@
+# Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso contrário escrever NÃO É MAIOR QUE 10
+def dvalor():
+    valor = int(input("Qual seu valor?"))
+    if  valor >=10:
+        print("Seu valor è maior ou igual a 10 !")
+    else:
+        print("Seu valor é menor que 10 !")
+dvalor()
+
+    
